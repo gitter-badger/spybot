d8dbcbeee8b2beac4e81597aeb1a124c4d7d8477
Joshua Zenn <wordman05@gmail.com>
2015-05-15 13:28:02 -0400

    Update entry.ino
    Base file.

--- src/entry.ino
+++ src/entry.ino
@@ -1,6 +1,28 @@
+int pin_status = 13;
+int pin_weapon1 = 12;
+
 void setup()
 {
-  // put your setup code here, to run once:
+  bool hasFailed = false;
+  // Initialize pins
+  pinMode(pin_status, OUTPUT);
+  pinMode(pin_weapon1, OUTPUT);
+  // Turn on status light
+  digitalWrite(pin_status, HIGH);
+  // Initialize vars
+  // Apply power to camera
+  // Turn off status light
+  digitalWrite(pin_status, LOW);
+  if(hasFailed)
+  {
+    while(true)
+    {
+      delay(1000);
+      digitalWrite(pin_status, HIGH);
+      delay(1000);
+      digitalWrite(pin_status, LOW);
+    }
+  }
 }
 
 void loop()
